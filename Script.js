e2553dcd35dfd6a3c01fabda53d1e14415b9a601
Rishi <116449371+Rishi713144@users.noreply.github.com>
2025-09-24 23:56:47 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -7,6 +7,17 @@ const images = [
     'https://i.pinimg.com/564x/f2/9b/ae/f29baed184ebf3c6fa25d82fc259d873.jpg',
     'https://w0.peakpx.com/wallpaper/809/437/HD-wallpaper-paolo-maldini-milan.jpg',
     'https://assets.realmadrid.com/is/image/realmadrid/1330190795142?$Mobile$&fit=wrap',
+    'https://www.shutterstock.com/editorial/image-editorial/MdTdA513N1zaA6y8MDkxMzA=/karim-benzema-real-madrid-celebrates-scoring-opening-440nw-10484600u.jpg',
+    'https://a.espncdn.com/photo/2020/0304/r674744_1296x1296_1-1.jpg',
+    'https://preview.redd.it/david-beckham-made-his-debut-for-manchester-united-32-years-v0-zbxrdf4k4jqd1.jpg?width=640&crop=smart&auto=webp&s=070c4d4f721791c4225b7183ba66522bee36a873',
+    'https://foto.sondakika.com/haber/2025/07/14/dogum-gununde-cuce-bireyleri-calistiran-yamal-18846835_5007_amp.jpg',
+    'https://www.fcbarcelona.com/fcbarcelona/photo/2023/09/25/08593578-2837-4af2-9235-e3a56e3db23c/2023-09-16_FCBvsBETIS_97.jpg',
+    'https://www.aljazeera.com/wp-content/uploads/2025/01/2025-01-21T220448Z_1474946714_UP1EL1L1PBY5X_RTRMADP_3_SOCCER-CHAMPIONS-SLB-BAR-REPORT-1737497355.jpg?resize=770%2C513&quality=80',
+    'https://www.economist.com/content-assets/images/20230121_BLP510.jpg',
+    'https://www.economist.com/content-assets/images/20230121_BLP510.jpg',
+    'https://www.fcbarcelona.com/fcbarcelona/photo/2020/09/06/c7355114-245c-48a3-b5fe-0cea11dc9379/SUAREZ-56-min.jpg',
+
+
 
 
 ];
@@ -20,4 +31,5 @@ function showRandomImage() {
     const imageElement = document.getElementById('randomImage');
     imageElement.src = images[randomIndex];
     imageElement.classList.add('show');
+
 }

59b6bd4c54d750ad53082778767526eb628687c9
Rishi <116449371+Rishi713144@users.noreply.github.com>
2025-09-25 00:53:27 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -14,8 +14,15 @@ const images = [
     'https://www.fcbarcelona.com/fcbarcelona/photo/2023/09/25/08593578-2837-4af2-9235-e3a56e3db23c/2023-09-16_FCBvsBETIS_97.jpg',
     'https://www.aljazeera.com/wp-content/uploads/2025/01/2025-01-21T220448Z_1474946714_UP1EL1L1PBY5X_RTRMADP_3_SOCCER-CHAMPIONS-SLB-BAR-REPORT-1737497355.jpg?resize=770%2C513&quality=80',
     'https://www.economist.com/content-assets/images/20230121_BLP510.jpg',
-    'https://www.economist.com/content-assets/images/20230121_BLP510.jpg',
+    'https://media.cnn.com/api/v1/images/stellar/prod/220930165943-01-neymar.jpg?c=16x9&q=h_833,w_1480,c_fill',
     'https://www.fcbarcelona.com/fcbarcelona/photo/2020/09/06/c7355114-245c-48a3-b5fe-0cea11dc9379/SUAREZ-56-min.jpg',
+    'https://s.yimg.com/ny/api/res/1.2/f_SI1D7ZhLzV2drrn948LA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNztjZj13ZWJw/https://media.zenfs.com/en/football_espana_articles_993/2f146b947dd93dc57899e2174a835bf8',
+    'https://platform.barcablaugranes.com/wp-content/uploads/sites/21/chorus/uploads/chorus_asset/file/25819505/2191173318.jpg?quality=90&strip=all&crop=0%2C0%2C100%2C99.987501562305&w=2400',
+    'https://a4.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0803%2Fr1205648_1296x729_16%2D9.jpg',
+    'https://i.guim.co.uk/img/media/695be08ccc972411834346ccb50f1b5d4411ba22/49_55_2451_1471/master/2451.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=a7e210f7b4721335a9eca1b7ee2d5660',
+    'https://media.licdn.com/dms/image/v2/D4E12AQHmcaBrpXzs0g/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1683063381087?e=2147483647&v=beta&t=uI25wJBqJOO7fcCNg2etXqte7kMJ5hAj7nKhozG35KQ',
+    'https://cdn.artphotolimited.com/images/60913d60bd40b85323893a87/1000x1000/der-beruehmte-johan-cruyff-vom-fc-barcelona.jpg',
+    'https://icdn.sempreinter.com/wp-content/uploads/2025/05/Lautaro-Martinez-Inter-Milan-4.jpg',
 
 
 
@@ -33,3 +40,4 @@ function showRandomImage() {
     imageElement.classList.add('show');
 
 }
+
